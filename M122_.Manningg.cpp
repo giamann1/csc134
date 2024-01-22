b40966e85f7353b1286b04f2cbe8930b129dcde5
giamann1 <145729311+giamann1@users.noreply.github.com>
2024-01-22 16:17:49 +0000

    Completed M1T2

---
+++ M122_.Manningg.cpp
@@ -0,0 +1,22 @@
+//CSC 134
+//M1T2
+//Gia Manning
+
+#include <iostream>
+using namespace std;
+
+int main () {
+    cout << "Hello World" << endl;
+    cout << "My name is Gia Manning" << "/n";
+    cout << "C++ " << endl;
+
+    // Ask a question
+    string name;
+    cout << "What's your name? " << endl;
+    cin >> name;
+
+    cout << "Nice to meet you, " << name << endl;
+
+    cout << endl << endl;
+    return 0;
+}
